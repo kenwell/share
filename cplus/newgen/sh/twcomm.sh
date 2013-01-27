5c0c42853b850296b66fea6ae67216b1e41904c2
kenwell <kenwell.ustc@gmail.com>
2013-01-27 13:21:41 +0800

    rm newgen

--- cplus/newgen/sh/twcomm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#Usage: twcomm <orderhost> <orderport> <tnovrhost> <tnovrport> <msgnum> <speed> <groupno> <bodysize>
-./twcomm 127.0.0.1 5011 127.0.0.1 5010 100000 1000 1 10
--- cplus/newgen/sh/twsuper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#Usage: twcomm <orderhost> <orderport> <tnovrhost> <tnovrport> <msgnum> <speed> <groupno> <bodysize>
-./twsuper 127.0.0.1 5011 127.0.0.1 5010 10000 40 500 100
